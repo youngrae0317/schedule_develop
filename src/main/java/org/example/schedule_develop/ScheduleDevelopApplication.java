a2c7b6d2dee0872884bcdf57097a3cfdbb37f7b7
youngrae0317 <isp1229@naver.com>
2025-08-07 12:14:19 +0900

    초기 세팅 (yml 수정 등)

---
+++ src/main/java/org/example/schedule_develop/ScheduleDevelopApplication.java
@@ -0,0 +1,13 @@
+package org.example.schedule_develop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleDevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleDevelopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/schedule_develop/ScheduleDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.schedule_develop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleDevelopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
