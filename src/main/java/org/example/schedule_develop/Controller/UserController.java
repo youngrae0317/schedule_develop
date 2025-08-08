857149678fa68ce7f85e9bf649b867e66d1b7f33
youngrae0317 <isp1229@naver.com>
2025-08-08 16:42:52 +0900

    유저 CRUD (LV2) 중 RU 구현

--- src/main/java/org/example/schedule_develop/Controller/UserController.java
+++ src/main/java/org/example/schedule_develop/Controller/UserController.java
@@ -8,16 +8,49 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
 
+    /**
+     * 유저 생성 (Lv2) -> CREATE
+     **/
     @PostMapping
     public ResponseEntity<UserResponseDto> save(@RequestBody UserRequestDto requestDto) {
         UserResponseDto userResponseDto = userService.saveUser(requestDto);
         return new ResponseEntity<>(userResponseDto, HttpStatus.CREATED);
+    }
+
+    /**
+     * 유저 전체 조회 (Lv2) -> READ
+     **/
+    @GetMapping
+    public ResponseEntity<List<UserResponseDto>> findAll() {
+        List<UserResponseDto> userList  = userService.findAll();
+
+        return new ResponseEntity<>(userList, HttpStatus.OK);
+    }
+
+    /**
+     * 유저 단일 조회 (Lv2) -> READ
+     **/
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDto> findById(@PathVariable Long id) {
+        UserResponseDto findUser = userService.findById(id);
+        return new ResponseEntity<>(findUser, HttpStatus.OK);
+    }
+
+    /**
+     * 유저 수정 (Lv2) -> UPDATE
+     **/
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponseDto> updateUser(@PathVariable Long id, @RequestBody UserRequestDto requestDto) {
+        UserResponseDto updateUserResponseDto = userService.updateUser(id, requestDto);
+        return new ResponseEntity<>(updateUserResponseDto, HttpStatus.OK);
     }
 
 }
--- src/main/java/org/example/schedule_develop/Service/UserService.java
+++ src/main/java/org/example/schedule_develop/Service/UserService.java
@@ -1,11 +1,14 @@
 package org.example.schedule_develop.Service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.schedule_develop.Dto.ScheduleResponseDto;
 import org.example.schedule_develop.Dto.UserRequestDto;
 import org.example.schedule_develop.Dto.UserResponseDto;
 import org.example.schedule_develop.Entity.User;
 import org.example.schedule_develop.Repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -20,4 +23,24 @@ public class UserService {
         return new UserResponseDto(savedUser);
     }
 
+    public List<UserResponseDto> findAll() {
+        return userRepository.findAll()
+                .stream()
+                .map(UserResponseDto::new)
+                .toList();
+    }
+
+    public UserResponseDto findById(Long id) {
+        User findUser = userRepository.findByIdOrElseThrow(id);
+        return new UserResponseDto(findUser);
+    }
+
+    public UserResponseDto updateUser(Long id, UserRequestDto requestDto) {
+        User findUser = userRepository.findByIdOrElseThrow(id);
+
+        findUser.userUpdate(requestDto);
+
+        return new UserResponseDto(findUser);
+
+    }
 }
