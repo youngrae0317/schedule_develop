873317e9acb942e75829db88dc0fc37d5aeff0bf
youngrae0317 <isp1229@naver.com>
2025-08-12 11:58:59 +0900

    ExceptionHandler 수정

--- src/main/java/org/example/schedule_develop/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/schedule_develop/exception/GlobalExceptionHandler.java
@@ -11,7 +11,12 @@ public class GlobalExceptionHandler {
     // Valid 유효성 검사 실패 시 발생하는 예외 처리
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException ex) {
-        return ResponseEntity.badRequest().body(ex.getMessage());
+        String errorMessage = ex.getBindingResult()
+                .getAllErrors()
+                .get(0)
+                .getDefaultMessage();
+
+        return ResponseEntity.badRequest().body(errorMessage);
     }
 
     // Service 로직에서 직접 발생시키는 예외 처리
